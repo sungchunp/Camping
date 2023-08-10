179863919acfa4795d75b304567a224adfa8e4fa
sc <sceland940403@gmail.com>
2023-08-10 17:56:01 +0900

    아이진짜

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component.Header';
+import { Route, Router, Routes } from 'react-router-dom';
+import MainNav from './component.MainNav';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <MainNav />
+
       
     </div>
   );
